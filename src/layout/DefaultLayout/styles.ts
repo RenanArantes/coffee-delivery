bb4e47e459cf2483e6a8cabf09a8f79af6aee475
RenanArantes <malevo18@gmail.com>
2022-09-21 09:13:45 -0300

    Default Layout padding

--- src/layout/DefaultLayout/styles.ts
+++ src/layout/DefaultLayout/styles.ts
@@ -3,4 +3,10 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
   max-width: 1440px;
   margin: 2rem auto;
+
+  padding: 0px 5px;
+
+  @media (max-width: 1440px) {
+    padding: 0px 32px;
+  }
 `
