3fbfbb5fb99cf410dc3df3ad970ff7b3ca47d4e2
RenanArantes <malevo18@gmail.com>
2022-09-23 09:18:33 -0300

    Success styles done

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,11 +1,66 @@
-import { Header } from '../../components/Header'
-import { SuccessContainer } from './styles'
+import {
+  AddressInfo,
+  ConfirmationTitleContainer,
+  DeliveryConfirmationContainer,
+  InfoConfirmationContainer,
+  PaymentInfo,
+  SubTitle,
+  SuccessContainer,
+  TimerInfo,
+  Title,
+} from './styles'
+import deliver from '../../assets/delivery_in_bike.png'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 
 export function Success() {
   return (
     <SuccessContainer>
-      <Header />
-      Success!!
+      <ConfirmationTitleContainer>
+        <Title>Uhu! Pedido confirmado</Title>
+        <SubTitle>
+          Agora é só aguardar que logo o café chegará até você
+        </SubTitle>
+      </ConfirmationTitleContainer>
+      <DeliveryConfirmationContainer>
+        <InfoConfirmationContainer>
+          <div>
+            <AddressInfo>
+              <span>
+                <MapPin size={15} weight="fill" />
+              </span>
+              <span>
+                <p>
+                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                </p>
+                <p>Farrapos - Porto Alegre, RS</p>
+              </span>
+            </AddressInfo>
+            <TimerInfo>
+              <span>
+                <Timer weight="fill" />
+              </span>
+              <span>
+                <p>Previsão de entrega</p>
+                <p>
+                  <strong>20 min - 30 min</strong>
+                </p>
+              </span>
+            </TimerInfo>
+            <PaymentInfo>
+              <span>
+                <CurrencyDollar weight="fill" />
+              </span>
+              <span>
+                <p>Pagamento na entrega</p>
+                <p>
+                  <strong>Cartão de Crédito</strong>
+                </p>
+              </span>
+            </PaymentInfo>
+          </div>
+        </InfoConfirmationContainer>
+        <img src={deliver} alt="Delivery" />
+      </DeliveryConfirmationContainer>
     </SuccessContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,118 @@
 import styled from 'styled-components'
 
-export const SuccessContainer = styled.div``
+export const SuccessContainer = styled.div`
+  @media (max-width: 1040px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    //margin-left: 32px;
+  }
+`
+
+export const ConfirmationTitleContainer = styled.div`
+  width: 100%;
+  margin-top: 80px;
+  margin-bottom: 40px;
+`
+export const Title = styled.h1`
+  color: ${(props) => props.theme['yellow-dark']};
+
+  font-family: 'Baloo 2', cursive;
+  font-size: 32px;
+  font-weight: 800;
+  line-height: 130%;
+`
+export const SubTitle = styled.p`
+  color: ${(props) => props.theme['base-subtitle']};
+
+  font-size: 20px;
+  line-height: 130%;
+`
+
+export const DeliveryConfirmationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    width: 492px;
+    height: 293px;
+  }
+
+  @media (max-width: 1040px) {
+    display: flex;
+    flex-direction: column;
+
+    justify-content: center;
+
+    margin: 32px auto;
+    img {
+      margin-top: 32px;
+    }
+  }
+`
+export const InfoConfirmationContainer = styled.div`
+  width: 526px;
+
+  @media (max-width: 1040px) {
+    max-width: 460px;
+  }
+
+  background: linear-gradient(
+        ${(props) => props.theme.background},
+        ${(props) => props.theme.background}
+      )
+      padding-box,
+    linear-gradient(to right, #dbac2c, #8047f8) border-box;
+  border-radius: 6px 36px;
+  border: 1px solid transparent;
+
+  div {
+    margin: 40px auto;
+  }
+`
+const InfoContainer = styled.div`
+  max-width: 354px;
+  height: 42px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  margin-bottom: 32px;
+
+  :last-child {
+    margin-bottom: 0px;
+  }
+
+  p {
+    margin-left: 12px;
+  }
+
+  span:first-child {
+    width: 32px;
+    height: 32px;
+    line-height: 38px;
+    border-radius: 50%;
+    text-align: center;
+
+    color: ${(props) => props.theme.white};
+  }
+`
+export const AddressInfo = styled(InfoContainer)`
+  span:first-child {
+    background: ${(props) => props.theme.purple};
+  }
+`
+
+export const TimerInfo = styled(InfoContainer)`
+  span:first-child {
+    background: ${(props) => props.theme.yellow};
+  }
+`
+
+export const PaymentInfo = styled(InfoContainer)`
+  span:first-child {
+    background: ${(props) => props.theme['yellow-dark']};
+  }
+`
